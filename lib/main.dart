a88d3ad8ee710bd8ab3802b83041000a1538e7c9
Kasper <kshn16@student.aau.dk>
2020-03-01 21:16:38 +0100

    add exercises for training session finished
    Could use a search bar. The training list is hardcoded. there are 10 or so. It needs to read from a file instead or use a database.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:training_app/training_utility/exercise.dart';
+import 'package:training_app/training_utility/exerciseList.dart';
 import 'localization/localization.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -20,6 +23,7 @@ class MyApp extends StatelessWidget {
         const Locale('dk', ''),
       ],
       theme: ThemeData(
+        primaryColor: Colors.blueAccent[300],
       ),
       home: MyHomePage(),
     );
@@ -36,6 +40,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (Exercise exercise) {
+              return ListTile(
+                title: Text(
+                  exercise.name,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile
+            .divideTiles(
+              context: context,
+              tiles: tiles,
+            )
+            .toList();
+            
+            return Scaffold(
+            appBar: AppBar(
+              title: Text('Chosen Exercises ' + DateTime.now().day.toString() +'/'
+              + DateTime.now().month.toString()),
+              
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+  static ExerciseList exerciseList = new ExerciseList(); 
+  final List<Exercise> _suggestions = exerciseList.getExercises();
+  final Set<Exercise> _saved = Set<Exercise>();
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+
+  
 
   void _addNewExercise() {
     setState(() {
@@ -48,27 +91,49 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  @override
+   Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemCount: _suggestions.length,
+      itemBuilder: (BuildContext _context, int i) {
+        
+        return _buildRow(_suggestions[i]);
+      }
+    );
+  }
+  Widget _buildRow(Exercise exercise) {
+    final bool alreadySaved = _saved.contains(exercise);
+    return ListTile(
+      title: Text(
+        exercise.name,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.add_box : Icons.add_box,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(exercise);
+          } else {
+            _saved.add(exercise);
+          }
+        });
+      },
+    );
+  }
+
+
+  @override                                 
   Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-      
-        title: Text(AppLocalizations.of(context).appBarTitle + ' extra string'),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Text(AppLocalizations.of(context).testTitle),
-        //child: Text(AppLocalizations.getLocale(context)),
-      ),
-      /*
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addNewExercise(),
-        tooltip: 'Add ',
-        child: Icon(Icons.add),
-      ),
-      */ // This trailing comma makes auto-formatting nicer for build methods.
+    return Scaffold (appBar: AppBar(
+      title: Text('Add your exercises'),
+      actions: <Widget>[
+        IconButton(icon: Icon(Icons.send), onPressed: _pushSaved),
+      ],
+    ),
+    body: _buildSuggestions(),
     );
   }
 }
---
+++ lib/training_utility/exercise.dart
@@ -0,0 +1,9 @@
+class Exercise {
+  final String name;
+
+  Exercise({
+    this.name,
+  });
+
+
+}
